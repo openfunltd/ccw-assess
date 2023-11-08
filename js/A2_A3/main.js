211048690b2ca32a0d5449d3c37c0cc25f380462
typingmonk <tmonk.hsieh@tmonk.tw>
2023-11-08 17:02:08 +0800

    Counting attendance, and oral/written interpellation #6

--- js/A2_A3/main.js
+++ js/A2_A3/main.js
@@ -14,39 +14,67 @@ async function main(tableId) {
   $(document).prop('title', `A2 A3 ${term}-${sessionPeriod} 外交及委員會出席及質詢`);
   $("#title").text(`A2 A3 ${term}-${sessionPeriod} 外交及國防委員會出席及質詢`);
 
-  meetings = await getMeetings(term, sessionPeriod, comtCd);
-  rowsData = []
-  for(const meeting of meetings) {
-    if (meeting.議事錄 === undefined){
-      rowsData.push([meeting.id, meeting.title, "", "", "", "", "", ""]);
-      continue;
-    }
-    let rowData = [];
-    rowData.push(formatDates(meeting.議事錄.時間));
-    rowData.push(meeting.title);
-    rowData.push(meeting.議事錄.出席委員.join(" "));
-    let oral = meeting.議事錄.口頭質詢;
-    oral = (oral === undefined) ? "" : oral.join(" ");
-    rowData.push(oral);
-    let written = meeting.議事錄.書面質詢;
-    written = (written === undefined) ? "" : written.join(" ");
-    rowData.push(written);
-    let leave = meeting.議事錄.請假委員;
-    leave = (leave === undefined) ? "" : leave.join(" ");
-    rowData.push(leave);
-    rowData.push(meeting.議事錄.主席);
-    let number_of_oral_queries = (oral === "") ? 0 : 1;
-    rowData.push(number_of_oral_queries);
-    rowsData.push(rowData);
-  }
-
   let comtLegislators = [];
+  let comtIndex = 0;
   const comtName = await getCommitteeName(comtCd);
   const legislators = await getLegislators(term);
+  const trBeforeHead0 = $("#before-point");
+  const tdHead1 = $("#head-row-1");
+  const tdHead2 = $("#head-row-2");
+  const tdHead3 = $("#head-row-3");
 
   for (const legislator of legislators) {
     const isComt = legislator.committee.includes(`第${term}屆第${sessionPeriod}會期：${comtName}`);
-    if (isComt) { comtLegislators.push(legislator.name); }
+    if (isComt) {
+      comtIndex = comtIndex + 1;
+      trBeforeHead0.before($(`<td class="dt-head-center" colspan="6">${comtIndex}</td>`));
+      tdHead1.append(`<td class="dt-head-center" colspan="6">${legislator.party}</td>`);
+      tdHead2.append(`<td class="dt-head-center" colspan="6">${legislator.name}</td>`);
+      tdHead3.append(`<td class="nosort">出席</td>`);
+      tdHead3.append(`<td class="nosort">請假</td>`);
+      tdHead3.append(`<td class="nosort">缺席</td>`);
+      tdHead3.append(`<td class="nosort">口頭質詢</td>`);
+      tdHead3.append(`<td class="nosort">書面質詢</td>`);
+      tdHead3.append(`<td class="nosort">書面採計</td>`);
+      comtLegislators.push(legislator.name);
+    }
+  }
+
+  meetings = await getMeetings(term, sessionPeriod, comtCd);
+  rowsData = []
+  for(const meeting of meetings) {
+    let rowData = [];
+    if (meeting.議事錄 === undefined){
+      rowData.push(meeting.id, meeting.title);
+      rowData.push(...Array(6 + comtLegislators.length * 6).fill(""));
+      rowsData.push(rowData);
+      continue;
+    }
+    let attendees = meeting.議事錄.出席委員.join(" ");
+    let oral = meeting.議事錄.口頭質詢;
+    oral = (oral === undefined) ? "" : oral.join(" ");
+    let written = meeting.議事錄.書面質詢;
+    written = (written === undefined) ? "" : written.join(" ");
+    let leaveList = meeting.議事錄.請假委員;
+    leaveList = (leaveList === undefined) ? "" : leaveList.join(" ");
+    let oralMax = (oral === "") ? 0 : 1;
+    rowData.push(formatDates(meeting.議事錄.時間), meeting.title, attendees);
+    rowData.push(oral, written, leaveList);
+    rowData.push(meeting.議事錄.主席);
+    for (const name of comtLegislators) {
+      const attended = (attendees.includes(name)) ? 1 : 0;
+      const leave = (leaveList.includes(name)) ? 1 : 0;
+      const absent = ([attended, leave] === [0, 0]) ? 1 : 0;
+      const hasOral = (oral.includes(name)) ? 1 : 0;
+      const hasWritten = (written.includes(name)) ? 1 : 0;
+      let writtenCount = 0;
+      if (oralMax > hasOral) {
+        writtenCount = (hasOral + hasWritten > oralMax) ? oralMax - hasOral : hasWritten;
+      }
+      rowData.push(attended, leave, absent, hasOral, hasWritten, writtenCount);
+    }
+    rowData.push(oralMax);
+    rowsData.push(rowData);
   }
 
   const table = $('#' + tableId).DataTable({
