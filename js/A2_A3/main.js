a24e7ee53d129cd690753685311557173ef0ed07
typingmonk <tmonk.hsieh@tmonk.tw>
2023-11-13 11:41:22 +0800

    Adjust dataTables.fixColumns from 2 to 1 #6

--- js/A2_A3/main.js
+++ js/A2_A3/main.js
@@ -92,7 +92,7 @@ async function main(tableId) {
   const table = $('#' + tableId).DataTable({
     keys: true,
     scrollX: true,
-    fixedColumns: {left: 2},
+    fixedColumns: {left: 1},
     columnDefs: [
         { orderable: false, targets: 'nosort' }
     ],
